46d1aefadfb99a4a9b6add1f5979e86a6a560b19
RamonJul <ramoncarlosjulian@gmail.com>
2019-03-09 12:36:39 -0500

    added the ajax calls to the front end and removed the indentation rule in the lint json

--- models/authors.js
+++ models/authors.js
@@ -17,6 +17,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
+    profilepic: DataTypes.STRING,
     password: {
       type: DataTypes.STRING,
       allowNull: false
