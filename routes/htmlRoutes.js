93c492b198ecc29609646ab8151b41b5303bf240
Josh LaRochelle <joshua.r.larochelle@gmail.com>
2019-03-15 21:10:51 -0400

    My brain is full of eff

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -73,7 +73,8 @@ router.get("/post/:id", function(req, res) {
         }
       }
       // console.log(commentObj);
-      res.render("comments", { post: filteredArray });
+      // res.render("comments", filteredArray[0]);
+      res.json(filteredArray);
     });
   });
 });
